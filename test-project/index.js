32d751739e3a9b7d4c45e95a1963b053ec509a88
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-08-03 18:35:40 +0900

    fix: simple library 버전 업데이트
    cjs, mjs 지원 추가

---
+++ test-project/index.js
@@ -0,0 +1,6 @@
+import { add, divide, multiply, subtract } from "simple-library-junnak";
+
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
