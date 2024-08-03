d0ac02f18043bf106201fa37acd42db73207d0df
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-08-03 19:14:07 +0900

    feat: next library 추가

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
-
+import { add } from "simple-library-junnak";
 export default function Home() {
+
+  const result = add(1,2);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
