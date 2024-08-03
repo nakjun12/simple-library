a054c31a1d1dbf96cc6c33791b6fc58d6accec4a
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-08-03 18:35:14 +0900

    feat folder 이름 수정 및 node_modules gitignore 추가

--- testproject/node_modules/simple-library-junnak/testproject/index.cjs
+++
@@ -1,6 +0,0 @@
-const { add, divide, multiply, subtract } = require("simple-library-junnak");
-
-console.log(add(1, 2));
-console.log(subtract(1, 2));
-console.log(multiply(1, 2));
-console.log(divide(1, 2));
--- testproject/node_modules/simple-library-junnak/testproject/index.mjs
+++
@@ -1,6 +0,0 @@
-import { add, divide, multiply, subtract } from "simple-library-junnak";
-
-console.log(add(1, 2));
-console.log(subtract(1, 2));
-console.log(multiply(1, 2));
-console.log(divide(1, 2));
